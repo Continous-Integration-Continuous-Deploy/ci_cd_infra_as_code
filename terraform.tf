77043b62a54dea358d117c8ebbde2fbab5546f07
Mohit Saini <63721227+Mohit020888@users.noreply.github.com>
2024-01-18 05:19:59 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -4,6 +4,6 @@ terraform {
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
     bucket = "tf-state-storage-a1e1e6a446goodf3f0570560f1e81be127a6a7d75"             # the bucket
-    region = "US East (N. Virginia) us-east-1"                                        # the region
+    region = "us-east-1"                                                              # the region
   }
 }
